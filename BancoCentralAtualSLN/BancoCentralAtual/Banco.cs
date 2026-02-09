20d8e823e42094e45745a937bd3ff216248fb871
Lilian0078 <lilian.a.nunes@aluno.senai.br>
2026-02-09 11:37:30 -0300

    Add files via upload

---
+++ BancoCentralAtualSLN/BancoCentralAtual/Banco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoCentralAtual
+{
+    internal class Banco
+    {
+        public string Nome {  get; set; }
+        public string CodigoBACEN { get; set; }
+    }
+}
--- BancoCentralAtualSLN/BancoCentralAtual/Program.cs
+++ BancoCentralAtualSLN/BancoCentralAtual/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BancoCentralAtual;
+
+static void Main(String[] args)
+{
+    //Criando o projeto banco
+    Banco meuBanco = new Banco();
+    meuBanco.Nome = "Banco do Brasil";
+    meuBanco.CodigoBACEN = "001";
+
+    //Conta Bancária
+    ContaBancaria contaOrigem = new ContaBancaria();
+    contaOrigem.Agencia = "1234";
+    contaOrigem.NumeroConta = "56789-0";
+
+    ContaBancaria contaDestino = new ContaBancaria();
+    contaDestino.Agencia = "4321";
+    contaDestino.NumeroConta = "98765-0";
+
+    //Criando o objeto Transaçãoujjj
+    Transacao minhaTransacao = new Transacao();
+    minhaTransacao.Valor = 250.00m;
+    minhaTransacao.Data = DateTime.Now;
+    minhaTransacao.Tipo = "PIX";
+    minhaTransacao.ContaOrigem = contaOrigem;
+    minhaTransacao.ContaDestino = contaDestino;
+
+    //Exibindo os dados na tela
+    Console.WriteLine("---REGISTRO BACEN---");
+    Console.WriteLine("Banco: " + meuBanco.Nome);
+    Console.WriteLine("Código: " + meuBanco.CodigoBACEN);
+    Console.WriteLine("------------------------");
+    Console.WriteLine("Conta Origem: " + contaOrigem.NumeroConta + " (Agência " + contaOrigem.Agencia + ")");
+    Console.WriteLine("Conta Destino: " + contaDestino.NumeroConta + " (Agência " + contaDestino.Agencia + ")");
+    Console.WriteLine("Tipo: " + minhaTransacao.Tipo);
+    Console.WriteLine("Valor: R$ " + minhaTransacao.Valor);
+    Console.WriteLine("Data: " + minhaTransacao.Data);
+}
+
