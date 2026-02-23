a39522c352fb8c819f66524ff8d729208021a6c7
Lilian0078 <lilian.a.nunes@aluno.senai.br>
2026-02-23 09:15:48 -0300

    commit1-23/02

--- BancoCentralAtualSLN/BancoCentralAtual/Program.cs
+++ BancoCentralAtualSLN/BancoCentralAtual/Program.cs
@@ -1,39 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using BancoCentralAtual;
+using static BancoCentralAtual.Transacao;
 
-static void Main(String[] args)
+class Program
 {
-    //Criando o projeto banco
-    Banco meuBanco = new Banco();
-    meuBanco.Nome = "Banco do Brasil";
-    meuBanco.CodigoBACEN = "001";
+    static void Main(string[] args)
+    {
+        TransacaoPix meuPix = new TransacaoPix();
+        meuPix.Valor = 15000;
+        meuPix.ChavePix = "contato@gmail.com";
 
-    //Conta Bancária
-    ContaBancaria contaOrigem = new ContaBancaria();
-    contaOrigem.Agencia = "1234";
-    contaOrigem.NumeroConta = "56789-0";
+        Transacaoted meuTed = new Transacaoted();
+        meuTed.Valor = 6000;
 
-    ContaBancaria contaDestino = new ContaBancaria();
-    contaDestino.Agencia = "4321";
-    contaDestino.NumeroConta = "98765-0";
-
-    //Criando o objeto Transaçãoujjj
-    Transacao minhaTransacao = new Transacao();
-    minhaTransacao.Valor = 250.00m;
-    minhaTransacao.Data = DateTime.Now;
-    minhaTransacao.Tipo = "PIX";
-    minhaTransacao.ContaOrigem = contaOrigem;
-    minhaTransacao.ContaDestino = contaDestino;
-
-    //Exibindo os dados na tela
-    Console.WriteLine("---REGISTRO BACEN---");
-    Console.WriteLine("Banco: " + meuBanco.Nome);
-    Console.WriteLine("Código: " + meuBanco.CodigoBACEN);
-    Console.WriteLine("------------------------");
-    Console.WriteLine("Conta Origem: " + contaOrigem.NumeroConta + " (Agência " + contaOrigem.Agencia + ")");
-    Console.WriteLine("Conta Destino: " + contaDestino.NumeroConta + " (Agência " + contaDestino.Agencia + ")");
-    Console.WriteLine("Tipo: " + minhaTransacao.Tipo);
-    Console.WriteLine("Valor: R$ " + minhaTransacao.Valor);
-    Console.WriteLine("Data: " + minhaTransacao.Data);
+        Console.WriteLine($"Pix de R$ {meuPix.Valor} é válido? {meuPix.Validar()}");
+        Console.WriteLine($"TED de R$ {meuTed.Valor} é válido? {meuTed.Validar()}");
+    }
 }
-
--- BancoCentralAtualSLN/BancoCentralAtual/Transacao.cs
+++ BancoCentralAtualSLN/BancoCentralAtual/Transacao.cs
@@ -3,27 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BancoCentralAtual.Transacao;
 
 namespace BancoCentralAtual
 {
     internal class Transacao
     {
-        public decimal Valor { get; set; }
-        public DateTime Data { get; set; }
-        public string Tipo { get; set; }
-        public ContaBancaria ContaOrigem { get; set; }
-        public ContaBancaria ContaDestino { get; set; }
-        //rr
-        private decimal _valor;
-        public decimal valor
+        public class transacao
         {
-            get { return _valor; }
-            set
+            protected decimal _valor;
+
+            public decimal Valor
             {
-                if (value > 0)
-                    _valor = value;
-                else
-                    Console.WriteLine("Erro: o valor da transaçãodeve ser positivo");
+                get { return _valor; }
+                set { if (value > 0) _valor = value; }
+            }
+
+            public DateTime Data { get; set; } = DateTime.Now;
+            public string ContaOrigem { get; set; }
+            public string ContaDestino { get; set; }
+
+            public virtual bool Validar()
+            {
+                return _valor > 0;
+            }
+        }
+
+        public class TransacaoPix : transacao
+        {
+            public string ChavePix { get; set; }
+
+            public override bool Validar()
+            {
+                return _valor > 0 && _valor <= 10000;
+            }
+        }
+
+        public class transacaoTed : transacao
+        {
+            public override bool Validar()
+            {
+                return _valor > 500;
+            }
+        }
+
+        public class Transacaoted : transacao
+        {
+            public override bool Validar()
+            {
+                //Regra de exemplo: TED deve ser maior que R$5.000 para fiscalização
+                return _valor > 5000;
             }
         }
     }
